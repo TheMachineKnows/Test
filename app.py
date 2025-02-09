fc5210156a0af061ce94128718dc00cbf9ebd64f
TheMachineKnows <mmachine81@gmail.com>
2025-02-09 16:14:57 +0100

    first commit with flask

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, g, request, render_template
+import sqlite3
+
+DATABASE = 'database.db'
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
